93780d9b7125f7931d3ae7849b665511ce426173
Bartek Błęcki <bartek@blecki.pl>
2019-09-10 17:25:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,40 +5,39 @@ const socketIo = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-const UsersService = require ('./UsersService');
+const UsersService = require('./UsersService');
 
-const usersService = new UsersService();
+const userService = new UsersService();
 
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-  socket.on('join', (name) => {
-    usersService.addUser({
+io.on('connection', function (socket) {
+  // klient nasłuchuje na wiadomość wejścia do czatu
+  socket.on('join', function (name) {
+    // użytkownika, który pojawił się w aplikacji zapisujemy do serwisu trzymającego listę osób w czacie
+    userService.addUser({
       id: socket.id,
       name
     });
+    // aplikacja emituje zdarzenie update, które aktualizuje informację na temat listy użytkowników każdemu nasłuchującemu na wydarzenie 'update'
     io.emit('update', {
-      users: usersService.getAllUsers()
+      users: userService.getAllUsers()
     });
   });
-});
 
-io.on('connection', (socket) => {
   socket.on('disconnect', () => {
-    usersService.removeUser(socket.id);
+    userService.removeUser(socket.id);
     socket.broadcast.emit('update', {
-      users: usersService.getAllUsers()
+        users: userService.getAllUsers()
     });
   });
-});
 
-io.on('connection', (socket) => {
-  socket.on('message', (message) => {
-    const {name} = usersService.getUserById(socket.id);
+  socket.on('message', function (message) {
+    const {name} = userService.getUserById(socket.id);
     socket.broadcast.emit('message', {
       text: message.text,
       from: name
@@ -46,6 +45,6 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
+server.listen(3000, function(){
   console.log('listening on *:3000');
 });
